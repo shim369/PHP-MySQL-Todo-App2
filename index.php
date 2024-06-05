93921c849506691bfa2f43f66cd50f7401f9847d
Student WZIEU <student@wzieu.pl>
2024-06-05 10:08:11 +0200

    Added new development feature

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
                     ?>
                 </tbody>
             </table>
+            <p>Dodatkowa zmiana krok 6.</p>
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
